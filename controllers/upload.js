8465b0804d93d3cbbc7bf3da674933f4fdb21eab
Fabio Buono <33349200+FabioBuono@users.noreply.github.com>
2018-02-11 01:46:08 +0100

    Add files via upload

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,7 +1,7 @@
 const formidable = require('formidable');
 const path = require('path');
 const fs = require('fs');
-
+const helper = require('../config/uploadHelper');
 
 // Multiple Files Upload Get 
 exports.getFileUpload = (req, res) => {
@@ -9,33 +9,7 @@ exports.getFileUpload = (req, res) => {
     res.render('account/upload', {
       user: req.user
     });
- } else res.redirect('/');
-};
-
-
-// Multiple Files Upload Post 
-exports.postFileUpload = async (req, res) => {
-  // create an incoming form object
-  var form = new formidable.IncomingForm();
-  // specify that we want to allow the user to upload multiple files in a single request
-  form.multiples = true;
-  // store all uploads in the /uploads directory
-  form.uploadDir = path.join(__dirname, '../uploads');
-  // every time a file has been uploaded successfully,
-  // rename it to it's orignal name
-  form.on('file', function(field, file) {
-    fs.rename(file.path, path.join(form.uploadDir, file.name));
-  });
-  // log any errors that occur
-  form.on('error', function(err) {
-    console.log('An error has occured: \n' + err);
-  });
-  // once all the files have been uploaded, send a response to the client
-  form.on('end', function() {
-    res.end('success');
-  });
-  // parse the incoming request containing the form data
-  form.parse(req);
+  } else res.redirect('/');
 };
 
 // Single Files Upload Get 
@@ -44,28 +18,22 @@ exports.getFileUploadOne = (req, res) => {
     res.render('account/uploaded', {
       user: req.user
     });
- } else res.redirect('/');
+  } else res.redirect('/');
 };
 
+// Multiple Files Upload Post 
+exports.postFileUpload = async (req, res) => {
+  await helper.upHelper(req, res, true);
+};
 // Single Files Upload Post 
 exports.postFileUploadOne = (req, res) => {
-// create an incoming form object
-var form = new formidable.IncomingForm();
-// parse the incoming request containing the form data
-form.parse(req);
+  helper.upHelper(req, res, false);
+}
 
-    form.on('fileBegin', function (name, file){
-        file.path = path.join(__dirname, '../uploads/'); 
-        file.path += file.name;
-    });
 
-    form.on('file', function (name, file){
-        console.log('Uploaded ' + file.name);
-    });
-    res.render('account/uploaded', {
-      user: req.user
-    });
-  }
+
+
+
 
 //https://evdokimovm.github.io/javascript/nodejs/expressjs/multer/2016/11/03/Upload-files-to-server-using-NodeJS-and-Multer-package-filter-upload-files-by-extension.html
 
