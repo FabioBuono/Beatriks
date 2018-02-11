9994b07efe953b6a849e7d6f22d9a3d520a2fccc
Fabio Buono <33349200+FabioBuono@users.noreply.github.com>
2018-02-11 21:35:41 +0100

    Update uploadHelper.js

--- helper/uploadHelper.js
+++ helper/uploadHelper.js
@@ -3,13 +3,16 @@ const path = require('path');
 const fs = require('fs');
 
 
-//Formidable UpLoad Helper
+/**********************************************/
+/**         Formidable UpLoad Helper         **/
+/**********************************************/
+
 exports.upHelper = (req, res, multiple) => {
   // create an incoming form object
   var form = new formidable.IncomingForm();
   // specify IF we want to allow the user to upload multiple files in a single request
   if (multiple) {
-    form.multiples = true;
+    form.multiples = multiple;
   }
   
   // store all uploads in the /uploads directory
@@ -20,17 +23,17 @@ exports.upHelper = (req, res, multiple) => {
 
   // log any errors that occur
   form.on('error', function (err) {
-    console.log('An error has occured: \n' + err);
-    /**** TO DO *****/
+    console.log('An error has occured: \n' + err);    /**** TO DO *****/
   });
   // once all the files have been uploaded, send a response to the client
   form.on('end', function () {
-    res.end('success');
+    res.end('success');    /**** TO DO *****/
   });
   // every time a file has been uploaded successfully,
   // rename it to it's orignal name
   form.on('file', function (field, file) {
     fs.rename(file.path, path.join(form.uploadDir, file.name));
+       /**** TO DO *****/
   });
 
 }
